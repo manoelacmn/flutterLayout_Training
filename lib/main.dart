e95b0812ff12c27867ae7b46ca2a663a1de931ae
manoelacmn <manoel.acmn@puccampinas.edu.br>
2023-05-17 17:01:52 -0300

    added isFavoriteButton

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,57 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount++;
+        _isFavorited = true;
+      }
+    });
+    }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [Container(
+      padding: const EdgeInsets.all(0),
+      child: IconButton(
+        padding: const EdgeInsets.all(0),
+        alignment: Alignment.centerRight,
+      icon: (_isFavorited
+        ? const Icon(Icons.star)
+        : const Icon(Icons.star_border)),
+        color: Colors.red[500],
+        onPressed: _toggleFavorite,
+      ),
+    ),
+      SizedBox(
+        width: 18,
+        child: SizedBox(
+          child: Text("$_favoriteCount")
+        )
+      )
+    ],
+    );
+    throw UnimplementedError();
+  }}
+
+
 
 Widget textSection = const Padding(
   padding: EdgeInsets.all(32),
@@ -57,11 +108,12 @@ Widget titleSection = Container(
           ],
         ),
       ),
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      const Text("41")
+      // Icon(
+      //   Icons.star,
+      //   color: Colors.red[500],
+      // ),
+      // const Text("41")
+      const FavoriteWidget(),
     ],
   ),
 );
